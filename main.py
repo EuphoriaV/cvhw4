4376106133828f51ea96f7350ac421e95a77b3ac
Ilya Kuznetsov <ilyakuznecov84@gmail.com>
2026-03-11 11:22:22 +0300

    added plots

--- main.py
+++ main.py
@@ -21,15 +21,12 @@ class VAE(nn.Module):
             nn.Conv2d(3, 64, 4, 2, 1),
             nn.BatchNorm2d(64),
             nn.ReLU(True),
-
             nn.Conv2d(64, 128, 4, 2, 1),
             nn.BatchNorm2d(128),
             nn.ReLU(True),
-
             nn.Conv2d(128, 256, 4, 2, 1),
             nn.BatchNorm2d(256),
             nn.ReLU(True),
-
             nn.Conv2d(256, 512, 4, 2, 1),
             nn.BatchNorm2d(512),
             nn.ReLU(True),
@@ -42,15 +39,12 @@ class VAE(nn.Module):
             nn.ConvTranspose2d(512, 256, 4, 2, 1),
             nn.BatchNorm2d(256),
             nn.ReLU(True),
-
             nn.ConvTranspose2d(256, 128, 4, 2, 1),
             nn.BatchNorm2d(128),
             nn.ReLU(True),
-
             nn.ConvTranspose2d(128, 64, 4, 2, 1),
             nn.BatchNorm2d(64),
             nn.ReLU(True),
-
             nn.ConvTranspose2d(64, 3, 4, 2, 1),
             nn.Tanh()
         )
